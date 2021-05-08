1061d34ea165459516245aaba07377bb521733a4
nicolas <nicolas.diarra@stellar.io>
2021-05-08 18:36:30 +0200

    feat(#1): Call to server

--- src/api/github/gitHub.js
+++ src/api/github/gitHub.js
@@ -13,20 +13,24 @@ function Api () {
 }
 
 export default {
-    getUserRepositories({username}){
-        return Api().get(`users/${username}/repos`)
-            .then(resp => resp.data)
-    },
-    getCommitsByRepository({username, repo}){
-        return Api().get(`repos/${username}/${repo}/commits`)
-            .then(resp => resp.data)
-    },
-    getBranchesByRepository({username, repo}){
-        return Api().get(`repos/${username}/${repo}/branches`)
-            .then(resp => resp.data)
-    },
-    getUserInfo({username}){
-        return Api().get(`users/${username}`)
-            .then(resp => resp.data)
+    // getUserRepositories({username}){
+    //     return Api().get(`users/${username}/repos`)
+    //         .then(resp => resp.data)
+    // },
+    // getCommitsByRepository({username, repo}){
+    //     return Api().get(`repos/${username}/${repo}/commits`)
+    //         .then(resp => resp.data)
+    // },
+    // getBranchesByRepository({username, repo}){
+    //     return Api().get(`repos/${username}/${repo}/branches`)
+    //         .then(resp => resp.data)
+    // },
+    // getUserInfo({username}){
+    //     return Api().get(`users/${username}`)
+    //         .then(resp => resp.data)
+    // },
+    getUserRepositories () {
+        return axios.get('http://localhost:1070/github')
+            .then(response => response.data)
     }
 }
