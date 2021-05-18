66e751c350f4dfd8c63b4ed3f9156d08b06f1df0
nicolas <nicolas.diarra@stellar.io>
2021-05-18 23:33:16 +0200

    feat: map personnal metadata

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <p>Not Found</p>
+    <section v-html="description" class="section is-main-section"></section>
   </div>
 </template>
 
 <script>
+import {information as me} from '../../src/me/information.js'
 export default {
-  name: 'AboutMe'
+  name: 'AboutMe',
+  data () {
+    return {
+      description: me.description
+    }
+  }
 }
 </script>
